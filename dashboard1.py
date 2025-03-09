dda0635cf58e8b1b359451e10b053822a5b5f362
nabilarisqi00 <nabilarisqir@gmail.com>
2025-03-09 21:31:38 +0700

    Update dashboard1.py

--- dashboard1.py
+++ dashboard1.py
@@ -23,7 +23,7 @@ st.write("Gathering Data")
 
 # Path ke File CSV
 customer_df = pd.read_csv("E-commerce-public-dataset/E-Commerce Public Dataset/customers_dataset.csv")
-geolocation_df = pd.read_csv("E-commerce-public-dataset/E-Commerce Public Dataset/geolocation_dataset.csv")
+geolocation_df = pd.read_csv("E-commerce-public-dataset/E-Commerce Public Dataset/geo.gz/geolocation_dataset.csv")
 order_reviews_df = pd.read_csv("E-commerce-public-dataset/E-Commerce Public Dataset/order_reviews_dataset.csv")
 order_payments_df = pd.read_csv("E-commerce-public-dataset/E-Commerce Public Dataset/order_payments_dataset.csv")
 order_items_df = pd.read_csv("E-commerce-public-dataset/E-Commerce Public Dataset/order_items_dataset.csv")
